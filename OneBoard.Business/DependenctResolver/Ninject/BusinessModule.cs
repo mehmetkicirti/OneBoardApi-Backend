7912092fa48513aded3abec13859441a8e0e27a9
mehmet <mehmetkicirti@hotmail.com>
2019-10-14 17:02:13 +0300

    To Added being dependency resolver folder into using with Ninject

---
+++ OneBoard.Business/DependenctResolver/Ninject/BusinessModule.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneBoard.Business.DependenctResolver.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
