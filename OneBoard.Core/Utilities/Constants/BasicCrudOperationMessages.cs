d871818baea417c928d60517b8e928efa15e4022
mehmet <mehmetkicirti@hotmail.com>
2019-10-18 22:31:24 +0300

    Constant message file place changed Business to Concrete

---
+++ OneBoard.Core/Utilities/Constants/BasicCrudOperationMessages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneBoard.Core.Utilities
+{
+    public static class BasicCrudOperationMessages
+    {
+
+        public static string SUCCESS_ADD= "Successfully added the entity";
+
+
+        public static string SUCCESS_UPDATE = "Successfully updated the entity";
+
+        public static string SUCCESS_DELETE = "Successfully deleted the entity";
+
+        public static string SUCCESS_GET_LİST = "The operation get related list of entity successfully";
+
+        public static string FAIL_GET_LİST = "FAIL_GET_LİST";
+
+        public static string FAIL_ADD = "FAIL_ADD";
+
+        public static string SUCCESS_GET_ID = "The operation get the entity successfully";
+
+        public static string FAIL_UPDATE = "FAIL_UPDATE";
+
+        public static string FAIL_DELETE = "FAIL_DELETE";
+
+
+
+       
+
+
+    }
+}
