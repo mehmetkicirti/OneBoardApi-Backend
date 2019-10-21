7faddeead0e5732503df8af6061aed668b2c2a1c
mehmet <mehmetkicirti@hotmail.com>
2019-10-21 16:49:01 +0300

    New Async methods was filled.

--- OneBoard.Core/Business/EFBaseService/EfBaseService.cs
+++ OneBoard.Core/Business/EFBaseService/EfBaseService.cs
@@ -1,26 +1,29 @@
 ﻿using OneBoard.Core.DataAccess;
-using OneBoard.Core.Entity;
 using OneBoard.Core.Utilities;
 using OneBoard.Core.Utilities.Results.Abstract;
 using OneBoard.Core.Utilities.Results.Concerete.Data;
 using OneBoard.Core.Utilities.Results.Concerete.Result;
+using OneBoard.Entities.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OneBoard.Core.Business.EFBaseService
 {
     public class EfBaseService<TDal, TEntity> :
         IService<TEntity> where TEntity : class, IEntity, new()
-        where TDal :IEntityRepository<TEntity>
+        where TDal : IEntityRepository<TEntity>
     {
 
         private readonly TDal _dal;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public EfBaseService(TDal dal)
+        public EfBaseService(TDal dal, IUnitOfWork unitOfWork)
         {
             _dal = dal;
+            _unitOfWork = unitOfWork;
         }
 
         public IResult Add(TEntity entity)
@@ -28,12 +31,13 @@ namespace OneBoard.Core.Business.EFBaseService
             try
             {
                 _dal.Add(entity);
+                _unitOfWork.Complete();
                 return new SuccessResult(BasicCrudOperationMessages.SUCCESS_ADD);
             }
 
             catch (Exception e)
             {
-                return new FailResult(e.Message);
+                return new FailResult($"{BasicCrudOperationMessages.FAIL_ADD} : {e.Message}");
             }
         }
 
@@ -42,18 +46,29 @@ namespace OneBoard.Core.Business.EFBaseService
             try
             {
                 _dal.Delete(entity);
+                //Here it is before completeasync execute method what ever you want.So that it is necessary. 
+                _unitOfWork.Complete();
                 return new SuccessResult(BasicCrudOperationMessages.SUCCESS_DELETE);
             }
 
             catch (Exception e)
             {
-                return new FailResult(e.Message);
+                return new FailResult($"{BasicCrudOperationMessages.FAIL_DELETE} : {e.Message}");
             }
         }
 
         public IDataResult<TEntity> GetById(int Id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return new SuccessDataResult<TEntity>(_dal.Find(Id),
+                    BasicCrudOperationMessages.SUCCESS_GET_ID);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
         }
 
         public IDataResult<IQueryable<TEntity>> GetEntityQueryable()
@@ -87,13 +102,109 @@ namespace OneBoard.Core.Business.EFBaseService
             try
             {
                 _dal.Update(entity);
+                _unitOfWork.Complete();
                 return new SuccessResult(BasicCrudOperationMessages.SUCCESS_UPDATE);
             }
 
             catch (Exception e)
             {
-                return new FailResult(e.Message);
+                return new FailResult($"{BasicCrudOperationMessages.FAIL_UPDATE} : {e.Message}");
             }
         }
+
+
+
+        #region async
+        public async Task<IResult> AddAsync(TEntity entity)
+        {
+            try
+            {
+                await _dal.AddAsync(entity);
+                await _unitOfWork.CompleteAsync();
+
+                return new SuccessDataResult<TEntity>(entity, BasicCrudOperationMessages.SUCCESS_ADD);
+            }
+            catch (Exception e)
+            {
+                return new FailResult(e.Message);
+            } 
+        }
+        public async Task<IResult> UpdateAsync(int Id)
+        {
+            try
+            {
+                TEntity entity = await _dal.FindByIdAsync(Id);
+                if (entity == null)
+                {
+                    return new FailDataResult<TEntity>($"{BasicCrudOperationMessages.NOT_FOUND_ENTİTY} :That belonging to {Id} any entity.");
+                }
+                else
+                {
+                    await _dal.UpdateAsync(Id);
+                    await _unitOfWork.CompleteAsync();
+                    return new SuccessResult(BasicCrudOperationMessages.SUCCESS_UPDATE);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new FailResult(ex.Message);
+            }
+        }
+        public async Task<IDataResult<IList<TEntity>>> GetEntityValuesAsync()
+        {
+            try
+            {
+                //filling entity list
+                IEnumerable<TEntity> entities = await _dal.ListAsync();
+                return new SuccessDataResult<IList<TEntity>>(entities.ToList(), $"{BasicCrudOperationMessages.SUCCESS_GET_LİST}");
+            }
+            catch (Exception ex)
+            {
+
+                return new FailDataResult<IList<TEntity>>($"There is an error.When it finding the list : {ex.Message}");
+            }
+        }
+
+        public async Task<IResult> DeleteByIdAsync(int Id)
+        {
+            try
+            {
+                TEntity entity = await _dal.FindByIdAsync(Id);
+                if(entity == null)
+                {
+                    return new FailDataResult<TEntity>($"{BasicCrudOperationMessages.NOT_FOUND_ENTİTY} :That belonging to {Id} any entity.");
+                }
+                else
+                {
+                    await _dal.DeleteByIdAsync(Id);
+                    await _unitOfWork.CompleteAsync();
+                    return new SuccessResult(BasicCrudOperationMessages.SUCCESS_DELETE);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return new FailResult($"{ex.Message}");
+            }
+        }
+
+        public async Task<IDataResult<TEntity>> FindByIdAsync(int Id)
+        {
+            try
+            {
+                TEntity entity = await _dal.FindByIdAsync(Id);
+                if(entity == null)
+                {
+                    return new FailDataResult<TEntity>($"{BasicCrudOperationMessages.NOT_FOUND_ENTİTY} :That belonging to {Id} any entity.");
+                }
+                return new SuccessDataResult<TEntity>(entity, $"{BasicCrudOperationMessages.SUCCESS_GET_ID}");
+            }
+            catch (Exception ex)
+            {
+                return new FailDataResult<TEntity>($"{BasicCrudOperationMessages.FIND_BY_ID_ERROR} :Exception : {ex.Message}");
+            }
+        }
+
+        #endregion
     }
 }
--- OneBoard.Core/Business/IService.cs
+++ OneBoard.Core/Business/IService.cs
@@ -1,9 +1,10 @@
-﻿using OneBoard.Core.Entity;
-using OneBoard.Core.Utilities.Results.Abstract;
+﻿using OneBoard.Core.Utilities.Results.Abstract;
+using OneBoard.Entities.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OneBoard.Core.Business
 {
@@ -20,5 +21,19 @@ namespace OneBoard.Core.Business
         IDataResult<TEntity> GetById(int Id);
 
         IDataResult<IQueryable<TEntity>> GetEntityQueryable();
+
+        #region async service
+        Task<IDataResult<IList<TEntity>>> GetEntityValuesAsync();
+
+        Task<IResult> AddAsync(TEntity entity);
+
+        //Task<IResult> DeleteAsync(TEntity entity);
+
+        Task<IResult> DeleteByIdAsync(int Id);
+
+        Task<IResult> UpdateAsync(int Id);
+
+        Task<IDataResult<TEntity>> FindByIdAsync(int Id);
+        #endregion
     }
 }
